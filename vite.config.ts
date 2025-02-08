d764d4317bc6bc6a715fdb348268ab9c3f8ae194
Shawn <OGShawnLee@gmail.com>
2025-02-08 14:06:17 +0000

    chore: configure unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import unocss from "unocss/vite";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), unocss()],
 })
